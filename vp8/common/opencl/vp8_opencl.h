d0faa521ac5da1472bb23cec98f4b83f2e00f9c0
Aaron Watry <awatry@gmail.com>
2011-02-28 20:06:37 -0500

    Enable memcpy kernel by default... It's actually faster than clEnqueueCopyBuffer.

--- vp8/common/opencl/vp8_opencl.h
+++ vp8/common/opencl/vp8_opencl.h
@@ -29,7 +29,7 @@ extern "C" {
 
 #define ENABLE_CL_IDCT_DEQUANT 0
 #define ENABLE_CL_SUBPIXEL 1
-#define MEM_COPY_KERNEL 0
+#define MEM_COPY_KERNEL 1
 #define ONE_CQ_PER_MB 1 //Value of 0 is racey... still experimental.
 #define ENABLE_CL_LOOPFILTER 0
 
